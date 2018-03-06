d03d4c5bd67776492c5e5d11cdde67a07d58b929
Shu Lin Chan <shulin.chan@gmail.com>
2018-03-06 13:56:39 -0500

    Clean up

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -11,31 +11,31 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
  * lab #1
  */
 public class Driver {
-	public static void main(String[] args) throws IOException,
-		ClassNotFoundException, InterruptedException {
-				
-		Path wiki0 = new Path(args[0]);
-		Path wiki1 = new Path(args[1]);
-		Path wiki2 = new Path(args[2]);
-		Path out = new Path(args[3]);
-		
-		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "name sorter");
-		
-		job.setInputFormatClass(TextInputFormat.class);
-		TextInputFormat.addInputPath(job, wiki0);
-		TextInputFormat.addInputPath(job, wiki1);
-		TextInputFormat.addInputPath(job, wiki2);
-		
-		job.setOutputFormatClass(TextOutputFormat.class);
-		TextOutputFormat.setOutputPath(job, out);
-		
-		job.setJarByClass(Driver.class);
-		job.setMapperClass(WordMapper.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		job.setReducerClass(WordReducer.class);
-		
-		job.waitForCompletion(true);
-	}
+  public static void main(String[] args) throws IOException,
+    ClassNotFoundException, InterruptedException {
+        
+    Path wiki0 = new Path(args[0]);
+    Path wiki1 = new Path(args[1]);
+    Path wiki2 = new Path(args[2]);
+    Path out = new Path(args[3]);
+    
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "name sorter");
+    
+    job.setInputFormatClass(TextInputFormat.class);
+    TextInputFormat.addInputPath(job, wiki0);
+    TextInputFormat.addInputPath(job, wiki1);
+    TextInputFormat.addInputPath(job, wiki2);
+    
+    job.setOutputFormatClass(TextOutputFormat.class);
+    TextOutputFormat.setOutputPath(job, out);
+    
+    job.setJarByClass(Driver.class);
+    job.setMapperClass(WordMapper.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    job.setReducerClass(WordReducer.class);
+    
+    job.waitForCompletion(true);
+  }
 }
--- src/main/java/WordReducer.java
+++ src/main/java/WordReducer.java
@@ -10,16 +10,16 @@ import org.apache.hadoop.mapreduce.Reducer;
  * lab #1
  */
 public class WordReducer extends Reducer<Text, Text, Text, Text> {
-	@Override
-	protected void reduce(Text key, Iterable<Text> values, Context ctx)
-			throws IOException, InterruptedException {
-		//Using a set so docIds are not repeated
-		Set<String> docIds = new HashSet<>();
-		for (Text t : values)
-			docIds.add(t.toString());
-		
-		Text docIdsCommaSeperated = new Text(String.join(",", docIds));
+  @Override
+  protected void reduce(Text key, Iterable<Text> values, Context ctx)
+      throws IOException, InterruptedException {
+    //Using a set so docIds are not repeated
+    Set<String> docIds = new HashSet<>();
+    for (Text t : values)
+      docIds.add(t.toString());
+    
+    Text docIdsCommaSeperated = new Text(String.join(",", docIds));
 
-		ctx.write(key, docIdsCommaSeperated);
-	}
+    ctx.write(key, docIdsCommaSeperated);
+  }
 }
