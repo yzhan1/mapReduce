1fa1048dabb9fe7a11e73c32f4183c1a4ed1c3db
Yaoming Zhan <zhanyaoming2014@gmail.com>
2018-04-12 18:26:50 -0400

    and is working

--- src/main/java/search/WordSearcher.java
+++ src/main/java/search/WordSearcher.java
@@ -1,36 +1,94 @@
 package search;
 
+import com.google.common.collect.Sets;
 import models.Word;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.ForeachFunction;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
+
+import java.util.*;
 
 public class WordSearcher {
   private SparkSession spark = SparkSession.builder().appName("WordSearcher").master("local[4]").getOrCreate();
 
   public WordSearcher() {
-    JavaRDD<Word> wordRDD = spark.read()
-      .textFile("./output/part-r-00000")
-      .javaRDD()
-      .map(line -> {
-        String[] parts = line.split("\\s+");
-        return new Word(parts[0], parts[1]);
-      }).cache();
+    Dataset<Row> wordDF;
+    try {
+      System.out.println("Reading from saved file");
+      wordDF = spark.read().parquet("./words.parquet");
+    } catch (Exception e) {
+      System.out.println("Loading from file");
 
-    Dataset<Row> wordDF = spark.createDataFrame(wordRDD, Word.class).cache();
+      JavaRDD<Word> wordRDD = spark.read()
+        .textFile("./output/part-r-00000")
+        .javaRDD()
+        .map(line -> {
+          String[] parts = line.split("\\s+");
+          return new Word(parts[0], parts[1]);
+        }).cache();
+
+      wordDF = spark.createDataFrame(wordRDD, Word.class).cache();
+      wordDF.write().parquet("./words.parquet");
+    }
+
     wordDF.createOrReplaceTempView("words");
   }
 
-  public void search(String term) {
-    Dataset<Row> resultsDF = spark.sql("SELECT * FROM words WHERE word LIKE CONCAT('%', '" + term + "', '%')");
-    Encoder<String> stringEncoder = Encoders.STRING();
+  public void search(String terms) {
+    String[] strings = terms.split("\\s+");
+    StringBuilder query = new StringBuilder();
+    query.append("SELECT * FROM words WHERE ");
+    boolean first = true;
+    for (String s : strings) {
+      if (s.equals("&") || s.equals("|") || s.equals("-")) {
+        continue;
+      }
+      if (!first) query.append("OR ");
+      query.append("word = '").append(s).append("' ");
+      first = false;
+    }
+    Dataset<Row> resultsDF = spark.sql(query.toString());
 
-    resultsDF.show();
-//    Dataset<String> searchResultsDF = resultsDF.map(
-//      (MapFunction<Row, String>) row -> "Word: " + row.getAs("word") + " Pos: " + row.getAs("positions"),
-//      stringEncoder
-//    );
-//    searchResultsDF.show();
+    List<Row> res = resultsDF.collectAsList();
+
+    List<Set<String>> map = new ArrayList<>();
+
+    res.forEach(row -> {
+      String[] positions = row.getAs("positions").toString().split(";");
+      Set<String> appearances = new HashSet<>();
+      for (String pos : positions) {
+        String docId = pos.substring(0, pos.indexOf("."));
+        appearances.add(docId);
+      }
+      map.add(appearances);
+    });
+
+    Set<String> result = map.get(0);
+
+    int positionCount = 0;
+    for (int i = 0; i < strings.length; i++) {
+      String current = strings[i++];
+      switch (current) {
+        case "&":
+          result = Sets.intersection(result, map.get(positionCount++));
+          break;
+        case "|":
+          result = Sets.union(result, map.get(positionCount++));
+          break;
+        case "-":
+          result = Sets.difference(result, map.get(positionCount++));
+          break;
+        default:
+          result = Sets.union(result, map.get(positionCount++));
+          i--;
+          break;
+      }
+    }
+
+    for (String val : result) {
+      System.out.println(val);
+    }
   }
 
   public void stop() {
@@ -39,7 +97,7 @@ public class WordSearcher {
 
   public static void main(String[] args) {
     WordSearcher searcher = new WordSearcher();
-    searcher.search("aaa");
+    searcher.search("abrar & ababa");
     searcher.stop();
   }
 }
