ead1aac70adf4151bd8e51d1d980f7fab248a583
Yaoming Zhan <zhanyaoming2014@gmail.com>
2018-04-23 00:13:48 -0400

    removed master local

--- src/main/java/search/SearchService.java
+++ src/main/java/search/SearchService.java
@@ -89,7 +89,7 @@ public class SearchService {
 //    }
 
     private String getFile(String word) {
-        StringBuilder sb = new StringBuilder("hdfs:///user/cs132g4/output10/part-r-00");
+        StringBuilder sb = new StringBuilder("/user/cs132g4/output10/part-r-00");
         int hash = WikiPartitioner.getHash(word, 676);
         String n = String.valueOf(hash);
         for (int i = n.length(); i < 3; i++) {
